0ee220c89d170d83e1baaaedfab2aa3c6b1ad51f
RosaliaNjoki <njosali0@gmail.com>
2023-12-23 00:48:38 +0300

    i completed the array lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+        const cats = ["Milo", "Otis", "Garfield"];
+        
+        
+        function destructivelyAppendCat(name){
+            cats.push("Ralph");
+            console.log(cats);
+
+         }
+
+         function destructivelyPrependCat(name){
+            cats.unshift("Bob");
+            console.log(cats);
+         }
+        
+        function destructivelyRemoveLastCat(name){
+            cats.pop("Ralph");
+            console.log(cats);
+         }
+
+        function destructivelyRemoveFirstCat(name){
+            cats.shift("Bob");
+            console.log(cats);
+         }
+         function appendCat(name){
+
+           var catt = [...cats, "Broom"];
+           return catt;
+        
+        }
+
+        function prependCat(name){
+           
+            var cat = ["Arnold", ...cats];
+            return cat;
+            
+
+        }
+
+        function removeLastCat(name){
+            
+            var caat = cats.slice(0, cats.length-1);
+            return caat;
+    
+
+        }
+       
+       
+        function removeFirstCat(name){
+         const cati = cats.slice(1);
+         return cati;
+            
+
+         }
+        
